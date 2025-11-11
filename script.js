6e0046ad77df193af219821647ba44f16aee68e3
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-11-11 14:26:28 +0530

    feat: add id parameter to Book constructor
    Each book now has unique identifier to prevent issues when books are
    removed or rearranged.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const myTracker = [];
 
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
