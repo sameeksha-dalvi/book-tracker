0d598c922797711daaf8a44e622378f5c05fff2f
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-11-12 17:47:37 +0530

    feat: add showBookData() to populate book data

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function Book(id, title, author, pages, read) {
     this.read = read;
 }
 
-Book.prototype.info = function(){
+Book.prototype.info = function () {
     return this.title + ", " + this.author + ", " + this.pages + " pages, " + this.read;
 }
 
@@ -19,13 +19,33 @@ Book.prototype.info = function(){
 
 //console.log(book1.info());
 
-function addBookToTracker(title,author,pages,read){
+function addBookToTracker(title, author, pages, read) {
     const id = crypto.randomUUID();
-    const book = new Book(id,title,author,pages,read);
+    const book = new Book(id, title, author, pages, read);
     myTracker.push(book);
-    console.log(myTracker);
+    //console.log(myTracker);
 }
 
-addBookToTracker('Alice in Wonderland','Lewis Carroll','250','not yet read');
-addBookToTracker('The Alchemist','Paulo Coelho','210','not yet read');
-addBookToTracker('Dune','Frank Herbert','412','not yet read');
+addBookToTracker('Alice in Wonderland', 'Lewis Carroll', '250', 'not yet read');
+addBookToTracker('The Alchemist', 'Paulo Coelho', '210', 'not yet read');
+addBookToTracker('Dune', 'Frank Herbert', '412', 'not yet read');
+
+
+function showBookData() {
+
+    const arrLength = myTracker.length;
+    const bookTitle = document.querySelector("#title1");
+    const bookAuthor = document.querySelector("#author1");
+    const bookPages = document.querySelector("#no_of_pages_1");
+    const readStatus = document.querySelector("#read_status_1");
+    bookTitle.textContent = myTracker[0].title;
+    bookAuthor.textContent = myTracker[0].author;
+    bookPages.textContent = myTracker[0].pages;
+    readStatus.textContent = myTracker[0].read;
+    //console.log(bookTitle.textContent);
+    // for (let i = 0; i < arrLength; i++) {
+    //     console.log("book" + i + " " + myTracker[i].title);
+    // }
+}
+
+showBookData();
