db34da4992b058123a820ba475d65d25744c4be5
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-11-14 16:40:00 +0530

    feat: add dynamic creation for one card element

--- script.js
+++ script.js
@@ -34,14 +34,46 @@ addBookToTracker('Dune', 'Frank Herbert', '412', 'not yet read');
 function showBookData() {
 
     const arrLength = myTracker.length;
-    const bookTitle = document.querySelector("#title1");
-    const bookAuthor = document.querySelector("#author1");
-    const bookPages = document.querySelector("#no_of_pages_1");
-    const readStatus = document.querySelector("#read_status_1");
+    const bookTitle = document.querySelector('#title1');
+    const bookAuthor = document.querySelector('#author1');
+    const bookPages = document.querySelector('#no_of_pages_1');
+    const readStatus = document.querySelector('#read_status_1');
     bookTitle.textContent = myTracker[0].title;
     bookAuthor.textContent = myTracker[0].author;
     bookPages.textContent = myTracker[0].pages;
     readStatus.textContent = myTracker[0].read;
+
+//create element
+
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+    cardDiv.textContent = 'Main Card Container';
+
+    const cardTitle = document.createElement('div');
+    cardTitle.className = 'card-title';
+    cardTitle.textContent = 'Card Title';
+
+    const cardAuthor = document.createElement('div');
+    cardAuthor.className = 'card-author';
+    cardAuthor.textContent = 'Card Author';
+
+    const cardPages = document.createElement('div');
+    cardPages.className = 'card-pages';
+    cardPages.textContent = 'Card Pages';
+
+    const cardRead = document.createElement('div');
+    cardRead.className = 'card-read';
+    cardRead.textContent = 'Card Read';
+
+    cardDiv.appendChild(cardTitle);
+    cardDiv.appendChild(cardAuthor);
+    cardDiv.appendChild(cardPages);
+    cardDiv.appendChild(cardRead);
+
+
+    document.querySelector('.cardcontainer').appendChild(cardDiv);
+
+
     //console.log(bookTitle.textContent);
     // for (let i = 0; i < arrLength; i++) {
     //     console.log("book" + i + " " + myTracker[i].title);
