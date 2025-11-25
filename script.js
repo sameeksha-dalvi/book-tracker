229ab1cdb13b8d7adf94eaa9a83d9b6e3783bd81
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-11-25 16:06:33 +0530

    feat: add unique IDs to cards

--- script.js
+++ script.js
@@ -49,18 +49,19 @@ function showBookData(bookArray) {
     for (let i = 0; i < arrLength; i++) {
         const cardDiv = document.createElement('div');
         cardDiv.className = 'card';
-
+        cardDiv.setAttribute('data-id',bookArray[i].id);
+        
         const cardDeleteDiv = document.createElement('div');
         cardDeleteDiv.className = 'card-delete';
 
         const svgNameSpace = "http://www.w3.org/2000/svg";
 
-        const svgDeleteIcon = document.createElementNS(svgNameSpace,"svg");
-        svgDeleteIcon.setAttribute('viewBox','0 0 24 24');
-        svgDeleteIcon.setAttribute('fill','currentColor');
+        const svgDeleteIcon = document.createElementNS(svgNameSpace, "svg");
+        svgDeleteIcon.setAttribute('viewBox', '0 0 24 24');
+        svgDeleteIcon.setAttribute('fill', 'currentColor');
 
-        const path = document.createElementNS(svgNameSpace,"path");
-        path.setAttribute("d","M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z");
+        const path = document.createElementNS(svgNameSpace, "path");
+        path.setAttribute("d", "M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z");
 
         svgDeleteIcon.appendChild(path);
         cardDeleteDiv.appendChild(svgDeleteIcon);
